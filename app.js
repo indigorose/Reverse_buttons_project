935e6e4357ffae6f0773524fddebac81758ee215
indigorose <indigorose049@gmail.com>
2023-11-27 13:41:21 +0000

    Update functionality and debugging

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-const array = [];
+// Create an array to store the values of the id to help with our color reverse.
+const btnIdArray = [];
 
-changeColor = () => {
-	const button = document.getElementById('1');
-	button.style.backgroundColor = 'Green';
-	setTimeout(() => {
-		button.style.backgroundColor = 'pink';
-	}, 3000);
+// Create document query selector to obtain buttons for event listener
+const buttons = document.querySelectorAll('btn');
+
+// Use a for loop to change each button, on each click
+
+for (const button of buttons) {
+	button.addEventListener('click', ChangeColor);
+}
+
+function ChangeColor(event) {
+	const btn = event.target;
+	btn.style.backgroundColor = 'green';
+	btnIdArray.push(btn.id);
+	console.log(btnIdArray);
+
+	if (btnIdArray.length == 6) {
+		ReverseButtons();
+	}
+}
+
+ReverseButtons = () => {
+	btnIdArray.reverse();
+
+	for (const id of btnIdArray) {
+		const reverseBtn = document.getElementById(id);
+		reverseBtn.style.backgroundColor = 'white';
+		btnIdArray = [];
+	}
 };
